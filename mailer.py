62ef94876a3850368155c9e5b65d7b6b406b5259
kunalmannu <112188096+kunalmannu@users.noreply.github.com>
2023-09-27 14:02:09 -0400

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -1,8 +1,11 @@
+#Made By KunalMannu
+#If You Copy Give Me Credits
+#Thank You
 from mailer import Mailer
 
-mail = Mailer(email='your hotmail account', password='your hotmail password')
+mail = Mailer(email='your hotmail account Example:hotkunalmannu@hotmail.com', password='your hotmail password Example:kunalmannuishot')
 mail.settings(provider=mail.MICROSOFT)
-mail.send(receiver='whom to send',
+mail.send(receiver='whom to send Example:hot@hotmail.com',
 subject='Your Subject', message='Your Message')
 
 print("Email Sents Succesfuly")
